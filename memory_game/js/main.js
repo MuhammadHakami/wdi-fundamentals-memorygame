9ff4659529d3a97a33e2c957c793a80d3c80bdc9
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-02-22 10:12:04 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-var cardOne="queen";
-var cardTwo = "king";
-var cardThree;
-var cardFour;
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardInPlay=[];
+var cards=["queen","queen","king","king"];
+var cardOne=cards[0];
+cardInPlay.push(cardOne);
+console.log("user flipped "+cardInPlay)
+var cardTwo=cards[2];
+cardInPlay.push(cardTwo)
+console.log("User flipped "+cardInPlay[1])
+if (cardInPlay.length===2) {
+  if (cardInPlay[0]===cardInPlay[1]) {
+    alert("You found a match!")
+  } else {
+    alert("Sorry, try again.")
+  }
+}
